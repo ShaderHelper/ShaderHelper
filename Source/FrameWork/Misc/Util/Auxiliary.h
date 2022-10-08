28133efa741b4d5654cc6a70006120231ccb61e3
mxrhyx233 <mxr233456@sina.com>
2022-10-09 07:15:58 +0800

    add outptr

--- Source/FrameWork/Misc/Util/Auxiliary.h
+++ Source/FrameWork/Misc/Util/Auxiliary.h
@@ -23,5 +23,81 @@ namespace FRAMEWORK {
 
 		};
 
+		template<typename Smart, typename Pointer>
+		struct OutPtrImpl;
+		
+		template<typename T, typename D, typename Pointer>
+		struct OutPtrImpl<TUniquePtr<T, D>, Pointer> {
+			using Smart = TUniquePtr<T, D>;
+			using Storage = typename Smart::ElementType*;
+		public:
+			OutPtrImpl(Smart& smart) : SmartPtr(smart), TargetPtr(nullptr) {}
+			operator Pointer* () {
+				return &TargetPtr;
+			}
+			~OutPtrImpl() {
+				SmartPtr.Reset(static_cast<Storage>(TargetPtr));
+			}
+		private:
+			Smart& SmartPtr;
+			Pointer TargetPtr;
+		};
+
+		template<typename T, ESPMode Mode, typename Pointer>
+		struct OutPtrImpl<TSharedPtr<T, Mode>, Pointer> {
+			using Smart = TSharedPtr<T, Mode>;
+			using Storage = typename Smart::ElementType*;
+		public:
+			OutPtrImpl(Smart& smart) : SmartPtr(smart), TargetPtr(nullptr) {}
+			operator Pointer* () {
+				return &TargetPtr;
+			}
+			~OutPtrImpl() {
+				Smart Temp = Smart(static_cast<Storage>(TargetPtr));
+				Swap(Temp, SmartPtr);
+			}
+		private:
+			Smart& SmartPtr;
+			Pointer TargetPtr;
+		};
+
+		template<typename T, ESPMode Mode, typename Pointer>
+		struct OutPtrImpl<TSharedRef<T, Mode>, Pointer> {
+			using Smart = TSharedRef<T, Mode>;
+			using Storage = typename Smart::ElementType*;
+		public:
+			OutPtrImpl(Smart& smart) : SmartPtr(smart), TargetPtr(nullptr) {}
+			operator Pointer* () {
+				return &TargetPtr;
+			}
+			~OutPtrImpl() {
+				Smart Temp = Smart(static_cast<Storage>(TargetPtr));
+				Swap(Temp, SmartPtr);
+			}
+		private:
+			Smart& SmartPtr;
+			Pointer TargetPtr;
+		};
+
+		template<typename Smart, typename Pointer,
+			std::enable_if_t<std::is_same_v<Pointer, void>, bool> = true
+		>
+		decltype(auto) BaseOutPtr(Smart& s) {
+			return OutPtrImpl<Smart, Smart::ElementType*>(s);
+		}
+
+		template<typename Smart, typename Pointer,
+			std::enable_if_t<!std::is_same_v<Pointer, void>, bool> = true
+		>
+		decltype(auto) BaseOutPtr(Smart& s) {
+			return OutPtrImpl<Smart, Pointer>(s);
+		}
+
+		//c++23 OutPtr for UE smart pointer.
+		template<typename Pointer = void, typename Smart>
+		decltype(auto) OutPtr(Smart& s) {
+			return BaseOutPtr<Smart,Pointer>(s);
+		}
+
 	}	
 }
