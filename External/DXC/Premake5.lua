bf80082235afb38ce8a265774f3f427aa4e946ba
mxrhyx233 <mxr233456@sina.com>
2023-04-03 13:52:53 +0800

    Improve the shader Compilation to generate pdb files.

--- External/DXC/Premake5.lua
+++ External/DXC/Premake5.lua
@@ -1,4 +1,12 @@
 usage "DXC"
     filter "system:windows"
 		postbuildcommands '{COPY} "%{wks.location}/External/DXC/bin/*.dll" "%{cfg.targetdir}"'
+        libdirs
+        {
+            path.getabsolute("./bin"),
+        }
+        includedirs
+        {
+            "./Inc",
+        }
 
--- Source/FrameWork/GpuApi/Win/D3D12Shader.cpp
+++ Source/FrameWork/GpuApi/Win/D3D12Shader.cpp
@@ -1,10 +1,11 @@
 #include "CommonHeader.h"
 #include "D3D12Shader.h"
-
+#include "Common/Path/PathHelper.h"
+#include <Misc/FileHelper.h>
 #pragma comment (lib, "dxcompiler.lib")
 
 namespace FRAMEWORK
-{
+{		
 	 DxcCompiler::DxcCompiler()
 	 {
 		 DxCheck(DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(Compiler.GetInitReference())));
@@ -14,31 +15,70 @@ namespace FRAMEWORK
 	 bool DxcCompiler::Compile(TRefCountPtr<Dx12Shader> InShader) const
 	 {
 		TRefCountPtr<IDxcBlobEncoding> BlobEncoding;
-		TRefCountPtr<IDxcOperationResult> OperationResult;
+		TRefCountPtr<IDxcResult> CompileResult;
 		auto SourceText = StringCast<ANSICHAR>(*InShader->GetSourceText());
 		const ANSICHAR* SourceTextPtr = SourceText.Get();
 		DxCheck(CompilerLibrary->CreateBlobWithEncodingFromPinned(SourceTextPtr, FCStringAnsi::Strlen(SourceTextPtr), CP_UTF8, BlobEncoding.GetInitReference()));
-		bool IsCompilationSucceeded = SUCCEEDED(Compiler->Compile(BlobEncoding,
-			*InShader->ShaderName, *InShader->EntryPoint, *InShader->ShaderTaget, 
-			nullptr, 0, nullptr, 0, nullptr, 
-			OperationResult.GetInitReference())
+
+		TArray<const TCHAR*> Arguments;
+		Arguments.Add(TEXT("/Qstrip_debug"));
+		Arguments.Add(TEXT("/E"));
+		Arguments.Add(*InShader->EntryPoint);
+		Arguments.Add(TEXT("/T"));
+		Arguments.Add(*InShader->ShaderTaget);
+#if DEBUG_SHADER
+		Arguments.Add(TEXT("/Zi"));
+#endif
+		
+		DxcBuffer SourceBuffer = { 0 };
+		SourceBuffer.Ptr = BlobEncoding->GetBufferPointer();
+		SourceBuffer.Size = BlobEncoding->GetBufferSize();
+		BOOL bKnown = 0;
+		uint32 Encoding = 0;
+		if (SUCCEEDED(BlobEncoding->GetEncoding(&bKnown, (uint32*)&Encoding)))
+		{
+			if (bKnown)
+			{
+				SourceBuffer.Encoding = Encoding;
+			}
+		}
+		bool IsApiSucceeded = SUCCEEDED(Compiler->Compile(&SourceBuffer,
+			Arguments.GetData(), Arguments.Num(), nullptr,
+			IID_PPV_ARGS(CompileResult.GetInitReference()))
 		);
 
-		if (IsCompilationSucceeded) 
+		bool IsCompilationSucceeded = IsApiSucceeded;
+
+		if (IsApiSucceeded)
 		{
-			TRefCountPtr<IDxcBlob> ShaderBlob;
-			DxCheck(OperationResult->GetResult(ShaderBlob.GetInitReference()));
-			InShader->SetCompilationResult(ShaderBlob);
-		}
-		else 
-		{
-			if (OperationResult.IsValid()) 
+
+			if (CompileResult.IsValid())
 			{
 				TRefCountPtr<IDxcBlobEncoding> BlobEncodingError;
-				OperationResult->GetErrorBuffer(BlobEncodingError.GetInitReference());
-				FString ErrorStr{ (int32)BlobEncodingError->GetBufferSize(), (ANSICHAR*)BlobEncodingError->GetBufferPointer()};
+				CompileResult->GetErrorBuffer(BlobEncodingError.GetInitReference());
+				FString ErrorStr{ (int32)BlobEncodingError->GetBufferSize(), (ANSICHAR*)BlobEncodingError->GetBufferPointer() };
+				if (!ErrorStr.IsEmpty())
+				{
+					SH_LOG(LogDx12, Error, TEXT("Shader compilation failed: %s"), *ErrorStr);
+					IsCompilationSucceeded = false;
+				}
+			}
 
-				SH_LOG(LogDx12, Error, TEXT("Shader compilation failed: %s"), *ErrorStr);
+			if (IsCompilationSucceeded)
+			{
+				TRefCountPtr<IDxcBlob> ShaderBlob;
+				DxCheck(CompileResult->GetResult(ShaderBlob.GetInitReference()));
+				InShader->SetCompilationResult(ShaderBlob);
+#if DEBUG_SHADER
+				TRefCountPtr<IDxcBlob> PdbBlob;
+				TRefCountPtr<IDxcBlobUtf16> PdbNameBlob;
+				DxCheck(CompileResult->GetOutput(DXC_OUT_PDB, IID_PPV_ARGS(PdbBlob.GetInitReference()), PdbNameBlob.GetInitReference()));
+				const FString PdbName = PdbNameBlob->GetStringPointer();
+				const FString PdbFile = PathHelper::SavedDir() + PdbName;
+				
+				TArray<uint8> BinaryData{ (uint8*)PdbBlob->GetBufferPointer(), (int32)PdbBlob->GetBufferSize() };
+				FFileHelper::SaveArrayToFile(BinaryData, *PdbFile);
+#endif
 			}
 		}
 		
