2dc73ccb94519afbc94b03389e8fc97a120e5f58
aierchoom <47219063+aierchoom@users.noreply.github.com>
2024-05-27 01:15:07 +0800

    Add Linux CI support and Linux UE lib download scripts. (#16)

---
+++ External/UE/downloadDepLinux.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+CurrentPath="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Downloading UE..."
+    UE_Lib_Dir="$CurrentPath/Lib"
+    UE_Lib_Url="https://gitlab.com/mxrhyx/shaderhelperdependency/uploads/472ff39cf6d8eca1c6e594cc248e92e3/UE-Lib-Linux.tar"
+    cd "$UE_Lib_Dir" || exit 1
+    wget $UE_Lib_Url || exit 1
+    tar -zxf UE-Lib-Linux.tar || exit 1
+    rm UE-Lib-Linux.tar
+
+    UE_Src_Dir="$CurrentPath/Src"
+    UE_Src_Url="https://gitlab.com/mxrhyx/shaderhelperdependency/uploads/f30840d7db9555d978d1019783976812/UE-Src.tar"
+    cd "$UE_Src_Dir" || exit 1
+    wget $UE_Src_Url || exit 1
+    tar -zxf UE-Src.tar || exit 1
+    rm UE-Src.tar
+echo
+
+exit 0
