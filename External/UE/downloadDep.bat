17e5954b8a32f616b9e0132b9ad72a84895fab08
SjMxr233 <mxr233456@sina.com>
2025-04-20 16:26:48 +0800

    Update dependencies

--- External/UE/downloadDep.bat
+++ External/UE/downloadDep.bat
@@ -4,7 +4,7 @@ set CURRENTDIR=%~dp0
 
 echo Downloading UE...
 	set UE-lib_Dir=%CURRENTDIR%/Lib
-	set UE-lib_Url="https://gitlab.com/-/project/55718299/uploads/d2126614b43ee7d382735f4e489ddbdd/UE-Lib-Win.zip"
+	set UE-lib_Url="https://gitlab.com/-/project/55718299/uploads/b95841417a47555e55bf09f754167e4f/UE-Lib-Win.zip"
 	pushd "%UE-lib_Dir%" || goto error
 	curl -LO %UE-lib_Url% || goto error
 	C:/Windows/System32/tar.exe -zxf UE-Lib-Win.zip || goto error
@@ -12,11 +12,11 @@ echo Downloading UE...
 	popd
 	
 	set UE-Src_Dir=%CURRENTDIR%/Src
-	set UE-Src_Url="https://gitlab.com/-/project/55718299/uploads/31b1f551b1050d9ce6dfa2a6bffb16d2/UE-Src.tar"
+	set UE-Src_Url="https://gitlab.com/-/project/55718299/uploads/144c0bc8e5d41f321f9fcf0d1b75ac68/UE-Src.tar.gz"
 	pushd "%UE-Src_Dir%" || goto error
 	curl -LO %UE-Src_Url% || goto error
-	C:/Windows/System32/tar.exe -zxf UE-Src.tar || goto error
-	del UE-Src.tar
+	C:/Windows/System32/tar.exe -zxf UE-Src.tar.gz || goto error
+	del UE-Src.tar.gz
 	popd
 echo.
 exit /b 0
--- External/UE/downloadDep.sh
+++ External/UE/downloadDep.sh
@@ -3,8 +3,8 @@ CurrentPath="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Downloading UE..."
     UE_Lib_Dir="$CurrentPath/Lib"
-    UE_Lib_Url0="https://gitlab.com/-/project/55718299/uploads/2e3026c2829e5945246c02392dea49d3/UE-Lib-Mac.zip.00"
-    UE_Lib_Url1="https://gitlab.com/-/project/55718299/uploads/35d36b61cb3aa5dd32c57de0cd8e223e/UE-Lib-Mac.zip.01"
+    UE_Lib_Url0="https://gitlab.com/-/project/55718299/uploads/ee1b49cc24eec8151ad3d37dfb045f6d/UE-Lib-Mac.zip.00"
+    UE_Lib_Url1="https://gitlab.com/-/project/55718299/uploads/ed3a254e0c91ef07b95ac6f659013c01/UE-Lib-Mac.zip.01"
     cd "$UE_Lib_Dir" || exit 1
     curl -LO $UE_Lib_Url0 || exit 1
     curl -LO $UE_Lib_Url1 || exit 1
@@ -13,11 +13,11 @@ echo "Downloading UE..."
     rm UE-Lib-Mac.zip*
 
     UE_Src_Dir="$CurrentPath/Src"
-    UE_Src_Url="https://gitlab.com/-/project/55718299/uploads/31b1f551b1050d9ce6dfa2a6bffb16d2/UE-Src.tar"
+    UE_Src_Url="https://gitlab.com/-/project/55718299/uploads/144c0bc8e5d41f321f9fcf0d1b75ac68/UE-Src.tar.gz"
     cd "$UE_Src_Dir" || exit 1
     curl -LO $UE_Src_Url || exit 1
-    tar -zxf UE-Src.tar || exit 1
-    rm UE-Src.tar
+    tar -zxf UE-Src.tar.gz || exit 1
+    rm UE-Src.tar.gz
 echo
 
 exit 0
