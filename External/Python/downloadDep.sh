27b5b0bb3685ec4cdacf189162992afb4a23e4b3
Mxr <mxr233456@sina.com>
2025-09-10 13:17:35 +0800

    Update downloadDep.sh

--- External/Python/downloadDep.sh
+++ External/Python/downloadDep.sh
@@ -2,7 +2,7 @@
 CurrentPath="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Downloading Python..."
-    Python_Dir="$CurrentPath/lib"
+    Python_Dir="$CurrentPath"
     Python_Url="https://gitee.com/mxr233/shader-helper-dependency/releases/download/v0.1/Python-Mac.zip"
     cd "$Python_Dir" || exit 1
     curl -LO $Python_Url || exit 1
